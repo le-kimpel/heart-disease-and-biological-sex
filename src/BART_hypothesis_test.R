3d8bfd1e879bb98f33de999cbb0cea2a2116a53b
Lauren Kimpel <55063664+le-kimpel@users.noreply.github.com>
2022-12-10 15:34:14 -0500

    Update BART_hypothesis_test.R
    Testing in other direction.

--- src/BART_hypothesis_test.R
+++ src/BART_hypothesis_test.R
@@ -8,16 +8,15 @@ dim(df)
 
 # build the train-and-test-set
 # Specify the binary treatment variables
-z <- df$sex
-x1 = df$target 
+X <- data.frame(df$sex)
+y = df$target 
 x2 = df$chol 
 x3 = df$thal
 
 x = cbind(x1, x2, x3)
-X = data.frame(x)
 
 # build BART regression model
-bart_machine = bartMachine(X,z)
+bart_machine = bartMachine(X,y)
 
 # obtain the hypothesized effect of X on y
 cov_importance_test(bart_machine, covariates = c(1))
