afc0435c61ea4fc626b34a37b667e51eef0aa2f6
Lauren Kimpel <55063664+le-kimpel@users.noreply.github.com>
2022-12-10 17:42:10 -0500

    Update BART_hypothesis_test.R

--- src/BART_hypothesis_test.R
+++ src/BART_hypothesis_test.R
@@ -3,20 +3,25 @@ library("bartMachine")
 # import heart disease data
 df <- read.csv("heart_disease\\heart.csv")
 
+
 # get the dimensions of our data
 dim(df)
 
 # build the train-and-test-set
 # Specify the binary treatment variables
 X <- data.frame(df$sex)
-y = df$target 
+x1 = df$target 
 x2 = df$chol 
 x3 = df$thal
+x4 = df$age
+x5 = df$trestbps
+x6 = df$cp
 
-x = cbind(x1, x2, x3)
+y = cbind(x1, x2, x3, x4, x5, x6)
 
-# build BART regression model
-bart_machine = bartMachine(X,y)
+#build BART regression model and get the hypothesized effect of X on each of the y_i
+for(i in 0:len(y)){
+  bart_machine = bartMachine(X,y[i])
+  cov_importance_test(bart_machine, covariates = c(1))
+}
 
-# obtain the hypothesized effect of X on y
-cov_importance_test(bart_machine, covariates = c(1))
