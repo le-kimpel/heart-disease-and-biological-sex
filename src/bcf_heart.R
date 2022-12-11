29500a2b335e2b946e72a12f426560c08683e62e
Lauren Kimpel <55063664+le-kimpel@users.noreply.github.com>
2022-12-11 11:17:34 -0500

    Update bcf_heart.R

--- src/bcf_heart.R
+++ src/bcf_heart.R
@@ -1,4 +1,6 @@
 library("bcf")
+library("dplyr")
+library("dbarts")
 
 # import heart disease data
 df <- read.csv("heart_disease\\heart.csv")
@@ -20,12 +22,14 @@ x3 = df$thal
 x4 = df$trestbps
 x5 = df$age
 
-x = cbind(x1, x2, x3, x4, x5)
-print(x)
+X = data.frame(cbind(x1, x2, x3, x4, x5))
+
+x = makeModelMatrixFromDataFrame(X)
 
 # create targeted selection: this will determine how aggressive our fit is
 # in general, the larger the value of q, the tighter the fit.
 q = -1*(x[,1]>(x[,2])) + 1*(x[,1]<(x[,2]))
+pihat = pnorm(q)
 
 # tau is the true (homogeneous) treatment effect
 tau = (0.5*(x[,5] > -3/4) + 0.25*(x[,5] > 0) + 0.25*(x[,5]>3/4))
@@ -34,14 +38,21 @@ tau = (0.5*(x[,5] > -3/4) + 0.25*(x[,5] > 0) + 0.25*(x[,5]>3/4))
 mu = (q + tau*z)
 
 # set the noise level relative to the expected mean function of Y
-sigma = 2*sd(q + tau)
+sigma = sd(q + tau*pihat)
 
 # draw the response variable with additive error!
 y = mu + sigma*rnorm(n)
 
-bcf_fit = bcf(y, z, x, x, pihat, include_pi="control", nburn=2000, nsim=2000)
+bcf_fit = bcf(y, z, x, x, pihat, include_pi="control", nburn=500, nsim=500)
 
-# Get posterior of treatment effects
 tau_post = bcf_fit$tau
 tauhat = colMeans(tau_post)
-plot(tau, tauhat); abline(0,1)
+
+print(tau)
+
+# generating plots 
+plot(tau , tauhat); abline(0,1)
+h1 <- hist(tauhat)
+#h2 <- hist(tau)
+plot(h1, col="lightblue")
+#plot(h2, col="red", add=TRUE)
