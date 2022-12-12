5d3d16920628b03d119b08925939e15ea25e91e5
Lauren Kimpel <55063664+le-kimpel@users.noreply.github.com>
2022-12-12 18:45:29 -0500

    Update bart_main.R

--- src/bart_main.R
+++ src/bart_main.R
@@ -17,17 +17,12 @@ x3 = df$thal
 x4 = df$trestbps
 x5 = df$age
 
-X = data.frame(cbind(x1, x2, x3, x4, x5))
-
-z = makeModelMatrixFromDataFrame(X)
-
-##generate Friedman data
 set.seed(11)
 
 ##build BART regression model
 
 ##Build another BART regression model
-bart_machine = bartMachine(x,z, num_trees = 200, num_burn_in = 500,
+bart_machine = bartMachine(x,x1, num_trees = 200, num_burn_in = 500,
                            num_iterations_after_burn_in = 1000)
 
 summary(bart_machine)
